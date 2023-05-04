6c82fb313f3ff606f52a52e9064c028d4678b6d9
Alisson Viegas <alissonviegas@acsiv.com.br>
2023-05-04 16:21:04 -0300

    Add fish shell configs

--- config.fish
+++ config.fish
@@ -8,16 +8,15 @@ if status is-interactive
     alias redis='redis-server'
     alias sidekiq='bundle exec sidekiq'
 
-    alias cpdotfiles='echo Copy .vimrc && cp ~/.vimrc ~/code/dotfiles/ &&
-                      echo Copy .gitconfig && cp ~/.gitconfig ~/code/dotfiles/ &&
-                      echo Copy .psqlrc && cp ~/.psqlrc ~/code/dotfiles/ &&
-                      echo Copy .pspgconf && cp ~/.pspgconf ~/code/dotfiles/ &&
-                      echo Copy config.fish && cp ~/.config/fish/config.fish ~/code/dotfiles/ &&
-                      echo Copy fish_plugins && cp ~/.config/fish/fish_plugins ~/code/dotfiles/ &&
-                      echo Copy fish_prompt && cp ~/.config/fish/functions/fish_prompt.fish ~/code/dotfiles/ &&
-                      echo Copy fish_functions && cp ~/.config/fish/functions/bat.fish ~/code/dotfiles/ &&
+    alias cpdotfiles='cp ~/.vimrc ~/code/dotfiles/ &&
+                      cp ~/.gitconfig ~/code/dotfiles/ &&
+                      cp ~/.psqlrc ~/code/dotfiles/ &&
+                      cp ~/.pspgconf ~/code/dotfiles/ &&
+                      cp ~/.config/fish/config.fish ~/code/dotfiles/ &&
+                      cp ~/.config/fish/fish_plugins ~/code/dotfiles/ &&
+                      cp ~/.config/fish/functions/fish_prompt.fish ~/code/dotfiles/ &&
+                      cp ~/.config/fish/functions/bat.fish ~/code/dotfiles/ &&
                       cd ~/code/dotfiles/ &&
-                      echo &&
                       git status -sb &&
                       echo &&
                       git diff &&
