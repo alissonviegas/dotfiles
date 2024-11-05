36b1c7105a3da816883afd7fec4ba8226a2d53a2
Alisson Viegas <alissonviegas@acsiv.com.br>
2024-11-05 12:50:10 -0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -6,16 +6,12 @@ if status is-interactive
     alias dry='ruby lib/dry.rb'
     alias pg='psql -U postgres acsiv_development'
     alias r='bin/dev'
-    alias redis='redis-server'
-    alias sidekiq='bundle exec sidekiq'
 
     alias cpdotfiles='cp ~/.vimrc ~/code/dotfiles/ &&
                       cp ~/.gitconfig ~/code/dotfiles/ &&
                       cp ~/.psqlrc ~/code/dotfiles/ &&
                       cp ~/.pspgconf ~/code/dotfiles/ &&
-                      cp ~/.config/fish/config.fish ~/code/dotfiles/ &&
-                      cp ~/.config/fish/fish_plugins ~/code/dotfiles/ &&
-                      cp ~/.config/fish/functions/fish_prompt.fish ~/code/dotfiles/ &&
+                      cp ~/.config/fish/config.fish ~/code/dotfiles/ &&                      
                       cp ~/.config/fish/functions/bat.fish ~/code/dotfiles/ &&
                       cd ~/code/dotfiles/ &&
                       git status -sb &&
