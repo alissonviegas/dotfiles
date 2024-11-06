5831482768fab49e57cbef4af06162cd0d36cc84
Alisson Viegas <alissonviegas@acsiv.com.br>
2024-11-06 14:47:06 -0300

    add alias

--- config.fish
+++ config.fish
@@ -6,12 +6,13 @@ if status is-interactive
     alias dry='ruby lib/dry.rb'
     alias pg='psql -U postgres acsiv_development'
     alias r='bin/dev'
+    alias rmprepush='rm .git/hooks/pre-push'
 
     alias cpdotfiles='cp ~/.vimrc ~/code/dotfiles/ &&
                       cp ~/.gitconfig ~/code/dotfiles/ &&
                       cp ~/.psqlrc ~/code/dotfiles/ &&
                       cp ~/.pspgconf ~/code/dotfiles/ &&
-                      cp ~/.config/fish/config.fish ~/code/dotfiles/ &&                      
+                      cp ~/.config/fish/config.fish ~/code/dotfiles/ &&
                       cp ~/.config/fish/functions/bat.fish ~/code/dotfiles/ &&
                       cd ~/code/dotfiles/ &&
                       git status -sb &&
