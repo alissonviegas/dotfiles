56b4fa88bfeb3e8df6996ab71705a66464f7ffa8
Alisson Viegas <alissonviegas@acsiv.com.br>
2025-09-15 14:50:47 -0300

    Install zoxide

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,12 +2,20 @@ if status is-interactive
     set -gx EDITOR vim
 
     ~/.rbenv/bin/rbenv init - fish | source
+    zoxide init fish | source
 
     # aliases
     alias dry='ruby lib/dry.rb'
     alias pg='psql -U postgres acsiv_development'
     alias r='bin/rails server -p 3000'
     alias rmprepush='rm .git/hooks/pre-push'
+
+    switch (uname)
+        case Darwin
+        case Linux
+            alias fd=fdfind
+    end
+
 
     cd code/acsiv
 end
