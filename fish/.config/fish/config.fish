b37c6d41409e0d66c4871fc5e6fb3506870e53ad
Alisson Viegas <alissonviegas@acsiv.com.br>
2025-09-15 12:05:10 -0300

    Remove cpdotfiles alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,16 +9,5 @@ if status is-interactive
     alias r='bin/rails server -p 3000'
     alias rmprepush='rm .git/hooks/pre-push'
 
-    alias cpdotfiles='cp ~/.vimrc ~/code/dotfiles/ &&
-                      cp ~/.gitconfig ~/code/dotfiles/ &&
-                      cp ~/.psqlrc ~/code/dotfiles/ &&
-                      cp ~/.config/fish/config.fish ~/code/dotfiles/ &&
-                      cp ~/.config/fish/functions/bat.fish ~/code/dotfiles/ &&
-                      cd ~/code/dotfiles/ &&
-                      git status -sb &&
-                      echo &&
-                      git diff &&
-                      echo'
-
     cd code/acsiv
 end
