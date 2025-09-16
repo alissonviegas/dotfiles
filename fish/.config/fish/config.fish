5182f387c50f5384fb16936a30861a787e6c0c06
Alisson Viegas <alissonviegas@acsiv.com.br>
2025-09-16 11:41:38 -0300

    Remove macos

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,21 +1,14 @@
 if status is-interactive
     set -gx EDITOR vim
 
+    ~/.rbenv/bin/rbenv init - fish | source
     zoxide init fish | source
 
     alias dry='ruby lib/dry.rb'
     alias pg='psql -U postgres acsiv_development'
     alias r='bin/rails server -p 3000'
     alias rmprepush='rm .git/hooks/pre-push'
-
-    switch (uname)
-        case Darwin
-            rbenv init - fish | source
-
-        case Linux
-            ~/.rbenv/bin/rbenv init - fish | source
-            alias fd=fdfind
-    end
+    alias fd=fdfind
 
     cd code/acsiv
 end
