85a08b5b5f54c65aeaddad66166b48263c04ebe4
Alisson Viegas <alissonviegas@acsiv.com.br>
2026-06-18 14:37:45 -0300

    Add y alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,10 +5,11 @@ if status is-interactive
     ~/.local/bin/mise activate fish | source
 
     alias dry='ruby lib/dry.rb'
+    alias fd=fdfind
     alias pg='psql -U alisson acsiv_development'
     alias r='bin/rails server -p 3000'
     alias rmprepush='rm .git/hooks/pre-push'
-    alias fd=fdfind
+    alias y='yarn install && yarn build && yarn build:css'
 
     cd ~/code/acsiv/
 end
